fbbfade88e4d5f4f7d03371116521e7b4856feca
adriano <adriano@adriano-A320M-S2H>
2019-12-18 20:10:02 -0300

    Adicionado caminho inicial para testes

--- src/main/java/br/com/siswbrasil/HomeController.java
+++ src/main/java/br/com/siswbrasil/HomeController.java
@@ -1,12 +1,14 @@
 package br.com.siswbrasil;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/")
 public class HomeController {
-	
+
+	@GetMapping
 	public String index() {
 		return "Olá mundo";
 	}
